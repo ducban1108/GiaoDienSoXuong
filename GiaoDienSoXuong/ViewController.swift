75928abb7f4dcc84485aa2fa6e25fe7b8186792c
Duc <levietduc1108@gmail.com>
2019-05-14 09:19:44 +0700

    Initial Commit

---
+++ GiaoDienSoXuong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiaoDienSoXuong
+//
+//  Created by Duc on 5/14/19.
+//  Copyright © 2019 Đức Đẹp Trai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
